783a47716a5944d3d42bcf24d35c2269e8e5d728
lumicrespo <50996757+lumicrespo@users.noreply.github.com>
2020-03-19 22:16:43 +0000

    tinha erro nos SIMB regs

--- source/kernel.c
+++ source/kernel.c
@@ -37,8 +37,8 @@ void volatile kernel(FLOAT *result, FLOAT *temp, FLOAT *power, int c_start, int 
 		 "ldr x2, [%[rc]]\n\t"
 		 "add x2, x1, x2\n\t"			//confirmar porque operando 2 = destino 
 		 "ldr q5, [%[temp], x2]\n\t"
-		 "fsub q6, q3, q5\n\t"
-		 "fmla q7, q6, q2\n\t"
+		 "fsub v6.4s, v3.4s, v5.4s\n\t"
+		 "fmla v7.4s, v6.4s, v2.4s\n\t"
 		 
 		 : [r] "=r" (result)
 		 : [c_s] "r" (&c_start), [Rx] "r" (Rx_1), [Ry] "r" (Ry_1), [Rz] "r" (Rz_1), [amb] "r" (&amb_temp), [ca] "r" (&Cap_1), [temp] "r" (temp), [rc] "r" (r_col)
