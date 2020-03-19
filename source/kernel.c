47a8a0f79455e70906e3441b748f197f1d1d1d3b
lumicrespo <50996757+lumicrespo@users.noreply.github.com>
2020-03-19 22:09:57 +0000

    ultima operação

--- source/kernel.c
+++ source/kernel.c
@@ -38,7 +38,7 @@ void volatile kernel(FLOAT *result, FLOAT *temp, FLOAT *power, int c_start, int 
 		 "add x2, x1, x2\n\t"			//confirmar porque operando 2 = destino 
 		 "ldr q5, [%[temp], x2]\n\t"
 		 "fsub q6, q3, q5\n\t"
-		 "fmla q6, q6, q2\n\t"			//confirmar porque operando 1 = destino 
+		 "fmla q7, q6, q2\n\t"
 		 
 		 : [r] "=r" (result)
 		 : [c_s] "r" (&c_start), [Rx] "r" (Rx_1), [Ry] "r" (Ry_1), [Rz] "r" (Rz_1), [amb] "r" (&amb_temp), [ca] "r" (&Cap_1), [temp] "r" (temp), [rc] "r" (r_col)
