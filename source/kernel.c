d6a839a46b732336ce88efb28d3193f033e02af3
lumicrespo <50996757+lumicrespo@users.noreply.github.com>
2020-03-23 18:56:58 +0000

    Update kernel.c

--- source/kernel.c
+++ source/kernel.c
@@ -44,10 +44,10 @@ void volatile kernel(float *result, float *temp, float *power, size_t c_start, s
 		 ".loop_neon:\n\t"
 		 "add x2, x1, x2\n\t"					//r*col+c
 		 "ldr q5, [%[temp], x2]\n\t"			//temp[r*col+c]
-		 "fsub v6.4s, v3.4s, v5.4s\n\t"			//v6 auxiliar, (amb_temp - temp[r*col+c])
-		 "str q6, [%[teste], x4]\n\t"
+		 "str q5, [%[teste], x4]\n\t"
 		 
 		 /*
+		 "fsub v6.4s, v3.4s, v5.4s\n\t"			//v6 auxiliar, (amb_temp - temp[r*col+c])
 		 "fmul v7.4s, v6.4s, v2.4s\n\t"			//v7 acumulador, (amb_temp - temp[r*col+c]) * Rz_1
 		 "sub x3, x2, #1 \n\t"					//r*col+c-1
 		 "ldr q8, [%[temp], x3]\n\t"			//v8 auxiliar, temp[r*col+c-1]
