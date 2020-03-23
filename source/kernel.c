7474979a55a138201da14227af9eb67df1106474
lumicrespo <50996757+lumicrespo@users.noreply.github.com>
2020-03-23 22:14:58 +0000

    Update kernel.c

--- source/kernel.c
+++ source/kernel.c
@@ -48,8 +48,6 @@ void volatile kernel(float *result, float *temp, float *power, size_t c_start, s
 		
 		 "fsub v6.4s, v3.4s, v5.4s\n\t"			//v6 auxiliar, (amb_temp - temp[r*col+c])
 		 "fmul v7.4s, v6.4s, v2.4s\n\t"			//v7 acumulador
-		  "str q7, [%[teste], x4]\n\t"
-		 /*
 		 "sub x3, x2, #4\n\t"					//r*col+c-1
 		 "ldr q8, [%[temp], x3]\n\t"			//v8 auxiliar, temp[r*col+c-1]
 		 "add x3, x2, #4 \n\t"					//r*col+c+1
@@ -67,7 +65,7 @@ void volatile kernel(float *result, float *temp, float *power, size_t c_start, s
 		 "ldr q6, [%[pow], x2]\n\t"				//v6 auxiliar, power[r*col+c]
 		 "fadd v8.4s, v6.4s, v7.4s\n\t"			//v8 auxiliar, acumulador(v7)+power[r+*col+c]
 		 "fmla v5.4s, v8.4s, v4.4s\n\t"			//result[r*col+c]
-		 
+		 "str q5, [%[teste], x4]\n\t"
 		 /*
 		 "add x1, x1, #16\n\t"					//c+4
 		 "cmp x1, %[sz]\n\t"
@@ -84,7 +82,7 @@ void volatile kernel(float *result, float *temp, float *power, size_t c_start, s
 		(temp[(r+1)*col+c] + temp[(r-1)*col+c] - 2.f*temp[r*col+c]) * Ry_1 + 
 		(temp[r*col+c+1] + temp[r*col+c-1] - 2.f*temp[r*col+c]) * Rx_1 + 
 		(amb_temp - temp[r*col+c]) * Rz_1));
-		printf("normal: %f, new: %f\n", (amb_temp - temp[r*col+c]) * Rz_1, teste[c-c_start]); 
+		printf("normal: %f, new: %f\n", teste1, teste[c-c_start]); 
 		
 	}
 	printf ("\n\n");
