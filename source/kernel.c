eeb5836f73c0e9b35f9d4d7f75bb63da9a9e45c0
lumicrespo <50996757+lumicrespo@users.noreply.github.com>
2020-03-23 21:35:30 +0000

    Update kernel.c

--- source/kernel.c
+++ source/kernel.c
@@ -84,11 +84,11 @@ void volatile kernel(float *result, float *temp, float *power, size_t c_start, s
 		
 	for (size_t c = c_start; c < c_start+4; ++c ) 
 	{
-		float teste1 =temp[r*col+c]+ ( Cap_1 * (power[r*col+c] + 
+		float teste =temp[r*col+c]+ ( Cap_1 * (power[r*col+c] + 
 		(temp[(r+1)*col+c] + temp[(r-1)*col+c] - 2.f*temp[r*col+c]) * Ry_1 + 
 		(temp[r*col+c+1] + temp[r*col+c-1] - 2.f*temp[r*col+c]) * Rx_1 + 
 		(amb_temp - temp[r*col+c]) * Rz_1));
-		if (teste != result[r*col+c])
+		if (teste!= result[r*col+c])
 		{
 
 			printf("ERROR\n");
