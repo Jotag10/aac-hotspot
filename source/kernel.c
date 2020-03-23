17222512901325d2d4743fd5246e70e6ff3f9ef8
lumicrespo <50996757+lumicrespo@users.noreply.github.com>
2020-03-23 11:48:36 +0000

    Update kernel.c

--- source/kernel.c
+++ source/kernel.c
@@ -44,10 +44,7 @@ void volatile kernel(float *result, float *temp, float *power, int c_start, int 
 		 
 		 ".loop_neon:\n\t"
 		 "add x2, x1, x2\n\t"					//r*col+c
-		 "sub x3, x2, #1 \n\t"					//teste
-		 "mov %[teste], x3\n\t"
-		 /*
-		 "ldr q5, [%[temp], x3]\n\t"			//temp[r*col+c]
+		 "ldr q5, [%[temp], x2]\n\t"			//temp[r*col+c]
 		 "fsub v6.4s, v3.4s, v5.4s\n\t"			//v6 auxiliar, (amb_temp - temp[r*col+c])
 		 "fmla v7.4s, v6.4s, v2.4s\n\t"			//v7 acumulador
 		 "sub x3, x2, #1 \n\t"					//r*col+c-1
@@ -71,9 +68,9 @@ void volatile kernel(float *result, float *temp, float *power, int c_start, int 
 		 "add x1, x1, #16\n\t"					//c+4
 		 "cmp x1, %[sz]\n\t"
          "b.lt .loop_neon\n\t"
-		*/
-		 //: [r] "=r" (result), 
-		 : [teste] "=r" (teste)
+		
+		 : [r] "=r" (result), 
+		 //: [teste] "=r" (teste)
 		 : [c] "r" (&c_start), [Rx] "r" (&Rx_1), [Ry] "r" (&Ry_1), [Rz] "r" (&Rz_1), [amb] "r" (&amb_temp), [ca] "r" (&Cap_1), [temp] "r" (temp),
 		 [pow] "r" (power), [rc] "r" (r_col), [col] "r" (col), [sz] "r" (iter*4)
 		 : "x1", "x2", "x3", "memory", "v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8", "v9"
