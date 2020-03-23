70fc8f92d25dcc92d7101c21e8aefbafd2e634d1
lumicrespo <50996757+lumicrespo@users.noreply.github.com>
2020-03-23 13:22:28 +0000

    Update kernel.h

--- source/kernel.h
+++ source/kernel.h
@@ -4,7 +4,7 @@
 #include <stddef.h>
 
 
-void volatile kernel(float *result, float *temp, float *power, int c_start, int size, int col, int r,
+void volatile kernel(float *result, float *temp, float *power, size_t c_start, size_t size, size_t col, size_t r,
 					  float Cap_1, float Rx_1, float Ry_1, float Rz_1, float amb_temp);
 
 
