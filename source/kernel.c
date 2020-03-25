dcc35af8fdf851facde80eb3ecb241733ac2a3a2
lumicrespo <50996757+lumicrespo@users.noreply.github.com>
2020-03-25 22:09:49 +0000

    Update kernel.c

--- source/kernel.c
+++ source/kernel.c
@@ -312,11 +312,13 @@ void volatile kernel(float *result, float *temp, float *power, size_t c_start, s
             (temp[(r+1)*col+c] + temp[(r-1)*col+c] - 2.f*temp[r*col+c]) * Ry_1 + 
             (temp[r*col+c+1] + temp[r*col+c-1] - 2.f*temp[r*col+c]) * Rx_1 + 
             (amb_temp - temp[r*col+c]) * Rz_1));
+			
     }
 	
 	/*CHECK VALUES */
 	for ( int c = c_start; c < size+c_start; ++c ) 
 	{
+		printf("LOOP: r*col+c: %d\n", r*col+c);
 		float teste1 =temp[r*col+c]+ ( Cap_1 * (power[r*col+c] + 
 			(temp[(r+1)*col+c] + temp[(r-1)*col+c] - 2.f*temp[r*col+c]) * Ry_1 + 
 			(temp[r*col+c+1] + temp[r*col+c-1] - 2.f*temp[r*col+c]) * Rx_1 + 
