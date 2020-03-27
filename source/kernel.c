bf554f674941e4dc134315929c07ac7dbb538feb
lumicrespo <50996757+lumicrespo@users.noreply.github.com>
2020-03-27 18:42:55 +0000

    Update kernel.c

--- source/kernel.c
+++ source/kernel.c
@@ -371,7 +371,7 @@ void volatile kernel(float *result, float *temp, float *power, size_t c_start, s
 		 
 		 "mov z6.d, p0/m, z3.d\n\t"							//auxiliar z6
 		 "fsub z3.d, p0/m, z3.d, z5.d\n\t"					//(amb_temp - temp[r*col+c])
-		 "st1w z3.d, p0, [%[teste], x4, lsl #2]\n\t"
+		 "st1w z5.d, p0, [%[teste], x4, lsl #2]\n\t"
 		 "fmul z3.d, p0/m, z3.d, z0.d\n\t"					//acumulador
 		 
 		 
