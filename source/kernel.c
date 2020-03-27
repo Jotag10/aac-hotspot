def1defbb81eb4f8e67b65e721d90ef3f0ad7615
lumicrespo <50996757+lumicrespo@users.noreply.github.com>
2020-03-27 21:48:58 +0000

    Update kernel.c

--- source/kernel.c
+++ source/kernel.c
@@ -374,7 +374,7 @@ void volatile kernel(float *result, float *temp, float *power, size_t c_start, s
 		 "fsub z6.s, p0/m, z6.s, z5.s\n\t"					//(amb_temp - temp[r*col+c])
 		 
 		 "fmul z6.s, p0/m, z6.s, z0.s\n\t"					//(amb_temp - temp[r*col+c])*Rx_1
-		 "st1w z6.s, p0, [%[teste], x4, lsl #2]\n\t"
+		 "st1sw z6.s, p0, [%[teste], x4, lsl #2]\n\t"
 		 
 	/*	 					
 		 
