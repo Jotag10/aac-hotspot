93ea8e826e05c85e9ef4dff2a5e78747592fef5c
lumicrespo <50996757+lumicrespo@users.noreply.github.com>
2020-03-29 19:02:55 +0100

    Update kernel.c

--- source/kernel.c
+++ source/kernel.c
@@ -385,7 +385,7 @@ void volatile kernel(float *result, float *temp, float *power, size_t c_start, s
 		 "ld1w { z8.s }, p0/z, [%[pow], x2, lsl #2]\n\t"	//z8, power[r*col+c]
 		 "fadd z8.s, p0/m, z8.s, z6.s\n\t"					//z8, acumulador(z6)+power[r+*col+c]
 		 "fmla z5.s, p0/m, z8.s, z4.s\n\t"					//z6 acumulador
-		 "st1w z5.s, p0, [%[res], x2, lsl #2]\n\t"
+		 //"st1w z5.s, p0, [%[res], x2, lsl #2]\n\t"
 		 "add x2, x2, #4\n\t"
 		 "incw x1\n\t"
 		 "whilelt p0.s, x1, %[sz]\n\t"
@@ -395,7 +395,7 @@ void volatile kernel(float *result, float *temp, float *power, size_t c_start, s
 		 : [res] "+r" (result), [teste] "+r" (teste)
 		 : [c] "r" (c_start), [Rx] "m" (Rx_1), [Ry] "m" (Ry_1), [Rz] "m" (Rz_1), [amb] "m" (amb_temp), [ca] "m" (Cap_1), [temp] "r" (temp),
 		 [pow] "r" (power), [r] "r" (r), [col] "r" (col), [sz] "r" (c_start+size-1)
-		 : "x1", "x2", "x3", "memory", "p0", "z0", "z1", "z2", "z3", "z4", "z5", "z6"
+		 : "x1", "x2", "x3", "memory", "p0", "z0", "z1", "z2", "z3", "z4", "z5", "z6", "z7", "z8", "z9"
 	);	
 	
 	for ( int c = c_start; c < size+c_start; ++c ) 
