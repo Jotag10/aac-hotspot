8a6ec28fd2c0a1407cd4fa9fe9adf1178e76766c
lumicrespo <50996757+lumicrespo@users.noreply.github.com>
2020-03-31 20:09:16 +0100

    Update kernel.h

--- source/kernel.h
+++ source/kernel.h
@@ -7,7 +7,7 @@
 void volatile kernel(float *result, float *temp, float *power, size_t c_start, size_t size, size_t col, size_t r,
 					  float Cap_1, float Rx_1, float Ry_1, float Rz_1, float amb_temp);
 					  
-void volatile kernel_ifs(float *result, float *temp, float *power, size_t c_start, size_t size, size_t col, size_t r,
+void volatile kernel_ifs(float *result, float *temp, float *power, size_t c_start, size_t size, size_t col, size_t r, size_t row,
 					  float Cap_1, float Rx_1, float Ry_1, float Rz_1, float amb_temp);
 
 #endif //KERNEL_AAC_HEADER__
