8d2f332eded7fed709d278909400c24bdb447c69
lumicrespo <50996757+lumicrespo@users.noreply.github.com>
2020-03-31 10:12:10 +0100

    Update hotspot.cpp

--- source/hotspot.cpp
+++ source/hotspot.cpp
@@ -77,62 +77,62 @@ void single_iteration(float *result, float *temp, float *power, int row, int col
             long long start_time_ifs = get_time();
             for ( r = r_start; r < r_start + BLOCK_SIZE_R; ++r ) {
                 for ( c = c_start; c < c_start + BLOCK_SIZE_C; ++c ) {
-
+					
                     if ( (r == 0) && (c == 0) ) {
                         delta = (Cap_1) * (power[0] +
                             (temp[1] - temp[0]) * Rx_1 +
                             (temp[col] - temp[0]) * Ry_1 +
                             (amb_temp - temp[0]) * Rz_1);
-						//printf("Corner1\n");
+						printf("Corner1\n");
                     }	
                     else if ((r == 0) && (c == col-1)) {
                         delta = (Cap_1) * (power[c] +
                             (temp[c-1] - temp[c]) * Rx_1 +
                             (temp[c+col] - temp[c]) * Ry_1 +
                         (   amb_temp - temp[c]) * Rz_1);
-						//printf("Corner2\n");
+						printf("Corner2\n");
                     }	
                     else if ((r == row-1) && (c == col-1)) {
                         delta = (Cap_1) * (power[r*col+c] + 
                             (temp[r*col+c-1] - temp[r*col+c]) * Rx_1 + 
                             (temp[(r-1)*col+c] - temp[r*col+c]) * Ry_1 + 
                         (   amb_temp - temp[r*col+c]) * Rz_1);	
-						//printf("Corner3\n");						
+						printf("Corner3\n");						
                     }	
                     else if ((r == row-1) && (c == 0)) {
                         delta = (Cap_1) * (power[r*col] + 
                             (temp[r*col+1] - temp[r*col]) * Rx_1 + 
                             (temp[(r-1)*col] - temp[r*col]) * Ry_1 + 
                             (amb_temp - temp[r*col]) * Rz_1);
-							//printf("Corner4\n");
+							printf("Corner4\n");
                     }	
                     else if (r == 0) {
                         delta = (Cap_1) * (power[c] + 
                             (temp[c+1] + temp[c-1] - 2.0*temp[c]) * Rx_1 + 
                             (temp[col+c] - temp[c]) * Ry_1 + 
                             (amb_temp - temp[c]) * Rz_1);
-							//printf("Edge1\n");
+							printf("Edge1\n");
                     }	
                     else if (c == col-1) {
                         delta = (Cap_1) * (power[r*col+c] + 
                             (temp[(r+1)*col+c] + temp[(r-1)*col+c] - 2.0*temp[r*col+c]) * Ry_1 + 
                             (temp[r*col+c-1] - temp[r*col+c]) * Rx_1 + 
                             (amb_temp - temp[r*col+c]) * Rz_1);
-							//printf("Edge2\n");
+							printf("Edge2\n");
                     }	
                     else if (r == row-1) {
                         delta = (Cap_1) * (power[r*col+c] + 
                             (temp[r*col+c+1] + temp[r*col+c-1] - 2.0*temp[r*col+c]) * Rx_1 + 
                             (temp[(r-1)*col+c] - temp[r*col+c]) * Ry_1 + 
                             (amb_temp - temp[r*col+c]) * Rz_1);
-							//printf("Edge3\n");
+							printf("Edge3\n");
                     }	
                     else if (c == 0) {
                         delta = (Cap_1) * (power[r*col] + 
                             (temp[(r+1)*col] + temp[(r-1)*col] - 2.0*temp[r*col]) * Ry_1 + 
                             (temp[r*col+1] - temp[r*col]) * Rx_1 + 
                             (amb_temp - temp[r*col]) * Rz_1);
-							//printf("Edge4\n");
+							printf("Edge4\n");
                     }
                     result[r*col+c] =temp[r*col+c]+ delta;
 					
