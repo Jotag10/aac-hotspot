fb2585967a3e78e2f2b0eef690704304a83531d2
lumicrespo <50996757+lumicrespo@users.noreply.github.com>
2020-04-05 19:43:35 +0100

    Update kernel.c

--- source/kernel.c
+++ source/kernel.c
@@ -544,11 +544,12 @@ void volatile kernel_ifs(float *result, float *temp, float *power, size_t c_star
 		
 		 ".sve_normal:\n\t"
 		 //x1 iterador c=c_start || c=1
+		 "whilelt p0.s, x1, %[sz]\n\t"
 		 "madd x2, %[r], %[col], x1\n\t"					//(r*col+c)
 		 //loop
 		 ".loop_sve_normal:\n\t"
 		 "ld1w { z1.s }, p0/z, [%[temp], x2, lsl #2]\n\t"	//z1, temp[r*col+c]
-		 "ld1rw {z2.s}, p0/z, [%[delta]]\n\t"					//z2, delta				
+		 "ld1rw {z2.s}, p0/z, [%[delta]]\n\t"				//z2, delta				
 		 "fadd z1.s, p0/m, z1.s, z2.s\n\t"					//temp[r*col+c]+delta
 		 "st1w z1.s, p0, [%[res], x2, lsl #2]\n\t"
 		 "incw x2\n\t"
