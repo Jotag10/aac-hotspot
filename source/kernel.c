5e83d558664f0a3a015bdbc62578018a051a9f3e
lumicrespo <50996757+lumicrespo@users.noreply.github.com>
2020-04-05 19:29:42 +0100

    Update kernel.c

--- source/kernel.c
+++ source/kernel.c
@@ -716,23 +716,18 @@ void volatile kernel_ifs(float *result, float *temp, float *power, size_t c_star
 	);
 	
 	
-	if (r!= 0)
+	
+	if (c_start==0)
 	{
-		if (r!= row-1)
-		{
-			if (c_start=0)
-			{
-				float teste_delta=(Cap_1) * (power[r*col] + 
-							(temp[(r+1)*col] + temp[(r-1)*col] - 2.0*temp[r*col]) * Ry_1 + 
-							(temp[r*col+1] - temp[r*col]) * Rx_1 + 
-							(amb_temp - temp[r*col]) * Rz_1);
-					
-				float teste_result=temp[r*col] + teste_delta;
-					
-				printf("%f, %f\n", result[r*col], teste_result);
-				printf("%f, %f\n\n", delta[0], teste_delta);
-			}
-		}
+		float teste_delta=(Cap_1) * (power[r*col] + 
+					(temp[(r+1)*col] + temp[(r-1)*col] - 2.0*temp[r*col]) * Ry_1 + 
+					(temp[r*col+1] - temp[r*col]) * Rx_1 + 
+					(amb_temp - temp[r*col]) * Rz_1);
+			
+		float teste_result=temp[r*col] + teste_delta;
+			
+		printf("%f, %f\n", result[r*col], teste_result);
+		printf("%f, %f\n\n", delta[0], teste_delta);
 	}
 	
 	
