49f00410108708a5da1ecae770a2bcf6524a3475
jorgesimoes <jorgesimoes+gitlab@tecnico.ulisboa.pt>
2020-04-18 19:09:01 +0100

    stuff

--- source/kernel.c
+++ source/kernel.c
@@ -615,7 +615,7 @@ void kernel_ifs(float *result, float *temp, float *power, size_t c_start, size_t
 		 
          "fmov z9.s ,p0/m, #2\n\t"
 		 //loop
-		 ".loop_sve_r_0:\n\t"
+		 /*".loop_sve_r_0:\n\t"
 		 "ld1w { z5.s }, p0/z, [%[temp], x1, lsl #2]\n\t"	//z5, temp[c]
 		 "mov z6.s, p0/m, z3.s\n\t"							//auxiliar z6
 		 "fsub z6.s, p0/m, z6.s, z5.s\n\t"					//z6, (amb_temp - temp[c])
@@ -640,7 +640,7 @@ void kernel_ifs(float *result, float *temp, float *power, size_t c_start, size_t
 		 "incw x1\n\t"
 		 "whilelt p0.s, x1, %[sz]\n\t"
 		 "b.first .loop_sve_r_0\n\t"
-		 
+		 */
 		 
 		//vê se é o CORNER
 		
